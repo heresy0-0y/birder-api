fb4022779e1cbbc38235c1bfe63190b8182ef551
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-05-07 11:08:48 -0500

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000', 'https://birder-learner.vercel.app'
+      origins 'http://localhost:3000', 'https://birder-learner.vercel.app', 'https://soundsalive.org', 'https://www.soundsalive.org'
   
       resource "*",
         headers: :any,
