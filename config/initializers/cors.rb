a12da52a3b3db017aa70b5d9ee7f631bf10d4d87
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-06-03 19:29:28 -0500

    cors update so favorites can be static props

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,5 +13,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
+  allow do
+    origins '*'
+    resource '/favorites',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
   end
   
